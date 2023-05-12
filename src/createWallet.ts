bb29f3a96b73d000f8f6dbcdd5f50b0e11d2d93e
Saviour1001 <harshghodkar@gmail.com>
2023-05-12 19:22:11 +0530

    added the function of creating wallets

---
+++ src/createWallet.ts
@@ -0,0 +1,44 @@
+import { TonClient } from "@eversdk/core";
+import { libNode } from "@eversdk/lib-node";
+// @ts-ignore
+import inquirer, { Answers, QuestionCollection } from "inquirer";
+import fs from "fs";
+
+export default async function createNewWallet() {
+  TonClient.useBinaryLibrary(libNode);
+  const client = new TonClient();
+  const keys = await client.crypto.generate_random_sign_keys();
+
+  console.log("Public key:", keys.public);
+
+  const questions = [
+    {
+      message: "Do you want to save the private key to a file?",
+      name: "save",
+      type: "confirm",
+    },
+  ];
+
+  const results = await inquirer.prompt(questions);
+  if (results.save) {
+    const questions2 = [
+      {
+        message: "Enter the file name",
+        name: "filename",
+        type: "input",
+      },
+    ];
+
+    const results2 = await inquirer.prompt(questions2);
+    console.log("Saving private key to file:", results2.filename);
+
+    // write it in a txt file
+    fs.writeFileSync(results2.filename + ".txt", keys.secret);
+
+    console.log("Private key saved to file:", results2.filename);
+  } else {
+    console.log("Private key not saved.");
+  }
+
+  client.close();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
+// @ts-ignore
+// const figlet = require('figlet');
+import figlet from "figlet";
+import createNewWallet from "./createWallet";
+const availableOptions: string[] = ["create", "deposit", "withdraw"];
 
-console.log(chalk.redBright("Hello world!"));
+// second argument should be the selected option
+const option: string = process.argv[2];
+
+if (!availableOptions.includes(option)) {
+  console.log(
+    `Invalid operation. Available operations are: ${availableOptions}`
+  );
+  process.exit(-1);
+}
+
+console.log(
+  chalk.magentaBright(
+    figlet.textSync(`Venom ${option}`, { horizontalLayout: "full" })
+  )
+);
+
+switch (option) {
+  case "create":
+    // arg 3 is the project name
+    console.log(chalk.magentaBright("create"));
+    createNewWallet();
+    break;
+  case "deposit":
+    console.log(chalk.magentaBright("deposit"));
+    break;
+  case "withdraw":
+    console.log(chalk.magentaBright("withdraw"));
+    break;
+}
